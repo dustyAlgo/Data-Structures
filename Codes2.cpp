5795ebd88f62af26130f2ed5eeb4ca9dee423c59
dustyAlgo <mrityunjaybhagat2002@gmail.com>
2025-10-21 01:10:32 +0530

    strings01

--- Codes2.cpp
+++ Codes2.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
 using namespace std;
-int main(){
-   vector<int> ans = {}
+char toLowercase(char ch){
+   if(ch >='a' && ch <='z'){
+      return ch;
+   }
+   else{
+      char temp = ch - 'A' + 'a';
+      return temp;
+   }
+}
+int main() {
+    string str;
+    cin >> str;   // reads a word (no spaces)
+
+    for (int i = 0; i < str.size(); i++) {
+        str[i] = toLowercase(str[i]);
+    }
+
+    cout << str;
+    return 0;
+}
