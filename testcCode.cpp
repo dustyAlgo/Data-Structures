f9af9ba04427a5a5d57415c78c84b3577c2bf848
dustyAlgo <mrityunjaybhagat2002@gmail.com>
2025-11-08 01:12:25 +0530

    changes11

--- testcCode.cpp
+++ testcCode.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
  int main(){
-    cout<<"hii";
+    int arr[5] = {12, 3, 4, 7, 10};
+    int minimum = *min_element(arr, arr+5);
+    cout<<minimum;
     return 0;
  }
